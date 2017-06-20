43fdc0894cc2e944cdc74adc2458a9db47634b73
Adolfo Moreno <adolfomoreno@Adolfos-MacBook-Pro.local>
2017-06-20 06:08:23 -0500

    Created mask

--- QBtracker.py
+++ QBtracker.py
@@ -13,7 +13,7 @@ while(1):
     # read our first frame
     frame = cap.read()
     # reduce frame size so that we can better see the changes
-
+    frame = imutils.resize(frame, width=600)
     # First method track using color detection
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
@@ -22,8 +22,15 @@ while(1):
     lower_red = np.array([150,150,50])
     upper_red = np.array([255,255,180])
 
+    # Create a mask with our hue values to single out color.
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+    # Erode our image to reduce noise level and single out our color.
+    mask = cv2.erode(mask, None, iterations = 2)
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+
     # show the frame
     cv2.imshow('frame',frame)
+    cv2.imshow('mask',mask)
 
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
